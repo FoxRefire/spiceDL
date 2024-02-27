84ec892d51de382fe603df87517b007745022174
FoxRefire <155989196+FoxRefire@users.noreply.github.com>
2024-02-28 04:18:59 +0900

    Add zotify backend(Closes #6)

--- README.md
+++ README.md
@@ -12,7 +12,17 @@ pip3 install spotdl flask flask_cors tkinter # Install dependencies
 python3 serve.py
 ```
 
+Alternatively, you can use zotify as downloading backend of music/podcast.
+Use this [alternative daemon](https://github.com/FoxRefire/spiceDL/blob/main/serve_zotify.py)
+```
+pip3 install spotdl flask flask_cors git+https://zotify.xyz/zotify/zotify.git
+python3 serve_zotify.py
+```
+
 
 ## Screenshots
 ![Screenshot1](https://github.com/FoxRefire/spiceDL/blob/Screenshot/Screenshot1.png?raw=true)
 ![Screenshot2](https://github.com/FoxRefire/spiceDL/blob/Screenshot/Screenshot2.png?raw=true)
+
+## Comparation of downloading backend
+![Comparation](https://github.com/FoxRefire/spiceDL/blob/Screenshot/comparition.png?raw=true)
---
+++ serve_zotify.py
@@ -0,0 +1,73 @@
+from flask import Flask
+from flask import Response
+from flask_cors import CORS
+from tkinter import Tk
+from tkinter.filedialog import askdirectory
+import uuid
+import json
+import subprocess
+
+app = Flask(__name__)
+cors = CORS(app)
+process={}
+
+@app.route('/spicedl/start/<string:name>/<path:url>')
+def start(name, url):
+    global process
+    proc_uuid = str(uuid.uuid4())
+    args=['zotify', url]
+
+    process[proc_uuid]={
+        "name": name,
+        "proc": subprocess.Popen(args)
+    }
+    return proc_uuid
+
+@app.route('/spicedl/remove/<string:uuid>')
+def remove(uuid):
+    global process
+    try:
+        process[uuid]["proc"].kill()
+    except:
+        pass
+    del process[uuid]
+    return "OK"
+
+@app.route('/spicedl/status')
+def status():
+    global process
+    ret={}
+    for k in process.keys():
+        if process[k]["proc"].poll() is None:
+            ret[k] = {
+                "name": process[k]["name"],
+                "status": 'Running'
+            }
+        else:
+            ret[k] = {
+                "name": process[k]["name"],
+                "status": "Finished" if process[k]["proc"].poll()==0 else "Failed"
+            }
+    return Response(json.dumps(ret), mimetype='application/json')
+
+@app.route('/spicedl/downloadDir/set')
+def downloadDirSet():
+    path = askdirectory(title='Select Folder')
+    with open('./spicedl.config', mode='w') as f: f.write(path)
+    return "OK"
+
+@app.route('/spicedl/downloadDir/get')
+def downloadDirGet():
+    try:
+        with open('./spicedl.config') as f: path = f.readline().rstrip()
+    except:
+        path=""
+    return path
+
+@app.route('/spicedl/downloadDir/reset')
+def downloadDirReset():
+    with open('./spicedl.config', mode='w') as f: f.write("")
+    return "OK"
+
+if __name__ == "__main__":
+    app.run(debug=True, port=8888, threaded=True)
